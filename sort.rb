841f7300f00d4c1b163017de95494979bb8bba58
Matt Lucas <mattlucas@gmail.com>
2013-12-12 11:24:20 -0500

    sort

---
+++ sort.rb
@@ -0,0 +1,51 @@
+# def matt_sort(item)
+  # sort_drawer = [] #this is for the merge
+  # you need to work in a while loop for a merge sort
+    # as a loop spin off each place in the array to be sorted into the new array
+    # check the length of the array to make sure they're equal
+
+  #get length of item to sort
+#   count = item.count
+
+#   # begin a new while loop
+#   i = 0
+
+#   while i < count do
+#   # check the first place against the second place to see which is bigger
+#     if item[i] > item[i+=1]
+#     # if the first place is bigger then move its place to the second spot and remove the first
+#       item[i].insert_at[i+=1]
+#       item.delete_at[i]
+#     # else leave it alone
+#     else
+#       i+=1
+#     end
+#   end
+#   # return the original array.
+#   return item
+
+#   # continue until it has nothing on the right to compare it to.
+
+# end
+
+
+
+def stack_overflow_sort(list)
+  # this is a tiny list that needs no sorting
+  return list if list.size <= 1 # already sorted
+  # sets a default for the while loop
+  swapped = true
+  while swapped do
+    # sets the list to false to see if it's done
+    swapped = false
+    # sets this to recursively work out the sort for any length
+    0.upto(list.length-2) do |i|
+      if list[i] > list[i+1]
+        list[i], list[i+1] = list[i+1], list[i] # swap values
+        swapped = true
+      end
+    end
+  end
+
+  list
+end
--- spec/sort_spec.rb
+++ spec/sort_spec.rb
@@ -1,2 +1,25 @@
 require 'spec_helper'
 require_relative '../sort'
+
+
+describe "Sort" do
+
+
+
+  it "matches the length of the array of objects" do
+    a = [1,2,7,9,3]
+    expect(a.count).to eq(stack_overflow_sort(a).count)
+  end
+
+  it "takes an array of objects" do
+    a = [1,4,2,7,11,9]
+    expect(a.class).to eq(stack_overflow_sort(a).class)
+  end
+
+  # it "expects item[i] to be smaller than item[i+1]" do
+  #   a = [1,2,3,7,9]
+  #   i=0
+  #   expect(a[i]).to_be <= (stack_overflow_sort(a).first)
+  # end
+
+end
